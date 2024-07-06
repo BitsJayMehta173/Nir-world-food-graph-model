48cd08f34c9b6f2eda6e14162cd9f5e0aab74b10
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2024-07-07 04:20:08 +0530

    corrections

--- starvation.py
+++ starvation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+dict=dict()
 class Node:
     def __init__(self, id, x, y, distance,value):
         self.id = id
@@ -10,25 +11,24 @@ class Node:
         self.value = value
         self.children = []
 
-def add_node(graph, parent_id, distance, angle_offset=0):
+def add_node(graph, parent_id, distance,i):
+
+    # for now we are adding all the new nodes with parent_id 0 but we need to optimize to save space by connecting it with nearest node with distance closer to center or its parent
+
     parent_node = graph[parent_id]
     
-    # Calculate the angle from the center to the parent node
-    if parent_node.id == 0:  # Special case for central node
+    # Calculate the angle from the center to the center node for now i have used random 0 to 360
+    angle = np.random.uniform(0, 2 * np.pi)
+    while angle in dict[0]:    
         angle = np.random.uniform(0, 2 * np.pi)
-    else:
-        center_node = graph[0]
-        dx = parent_node.x - center_node.x
-        dy = parent_node.y - center_node.y
-        angle = np.arctan2(dy, dx)
-    
-    # Add an offset angle for the new node and add angle_offset which is opposite of center and parent node which will be (0 to 180) (0,np.pi/2)
-    angle += angle_offset
-    
-    # Place the new node at the specified distance from the parent node along the calculated angle
+    # dict[i].append(angle)
+    dict[0].append(angle)
+
+
+    # Place the new node at the specified distance from the center node along the calculated angle
     new_x = parent_node.x + distance * np.cos(angle)
     new_y = parent_node.y + distance * np.sin(angle)
-    value=40
+    value=10
     new_node_id = len(graph)
     new_node = Node(new_node_id, new_x, new_y, distance,value)
     graph[new_node_id] = new_node
@@ -59,17 +59,22 @@ def draw_graph(graph):
 graph = {0: Node(0, 0, 0, 0,10)}
 
 # taken 10 nodes in the area of circle
-num_initial_nodes = 10
-initial_distances = [5, 7, 10, 6, 9, 8, 7, 10, 6, 8]
+# num_initial_nodes = 10
+# initial_distances = [5, 7, 10, 6, 9, 8, 7, 10, 6, 8]
 
 # Add initial nodes from the central node
-for distance in initial_distances:
-    add_node(graph, 0, distance)
-
-# Add more nodes dynamically and as we have added angle between 0 to 1
-add_node(graph, 1, 3, np.random.uniform(0,np.pi/2))  # Add a node 3 units away from node 1 with an angle offset of pi/4
-add_node(graph, 2, 4, np.random.uniform(0,np.pi/2))  # Add a node 4 units away from node 2 with an angle offset of -pi/3
-add_node(graph, 3, 2, np.random.uniform(0,np.pi/2))  # Add a node 2 units away from node 3 with an angle offset of pi/2
+# for now i am using loop to create new as nodes as much as i like with the same distance multiplied by range of random number between (index-1 to index) of loop which creates 
+maxdist=1
+dict[0]=[]
+for i in range(1,3):
+    # dict[i-1]=[]
+    dist=maxdist
+    for j in range(10):
+        distance=np.random.uniform(dist+30,dist+100)
+        add_node(graph, 0,distance,i-1)
+        if distance>maxdist:
+            maxdist=distance
+        
 
 # Draw the graph
 draw_graph(graph)
